0b1ab69e67d8c350716b925f69f2a107088b8673
sam-c-f <sam_friedman@hotmail.co.uk>
2022-09-05 14:40:44 +0100

    added test back to main as waws removed in last pull request

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -33,6 +33,45 @@ describe("/api/topics", () => {
   });
 });
 
+describe("/api/articles", () => {
+  describe("GET api/articles/:article_id", () => {
+    it("200: it responds with an article object all article properties and author", () => {
+      return request(app)
+        .get("/api/articles/1")
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.articles).toEqual(
+            expect.objectContaining({
+              article_id: 1,
+              title: "Living in the shadow of a great man",
+              topic: "mitch",
+              author: "jonny",
+              body: "I find this existence challenging",
+              created_at: "2020-07-09T20:11:00.000Z",
+              votes: 100,
+            })
+          );
+        });
+    });
+    it("404: id number not found", () => {
+      return request(app)
+        .get("/api/articles/20")
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.msg).toBe("not found");
+        });
+    });
+    it("400: wrong data type in request", () => {
+      return request(app)
+        .get("/api/articles/one")
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.msg).toBe("bad request");
+        });
+    });
+  });
+});
+
 describe("/api/users", () => {
   describe("GET", () => {
     it("responds with an array of users", () => {
