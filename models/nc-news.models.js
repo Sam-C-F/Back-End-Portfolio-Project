83d297ee4b84a4554fdf62b69b06ad7ba02578b8
sam-c-f <sam_friedman@hotmail.co.uk>
2022-09-08 15:41:35 +0100

    refactored to async/await syntax

--- models/nc-news.models.js
+++ models/nc-news.models.js
@@ -1,19 +1,16 @@
 const db = require("../db/connection");
 const endpoints = require("../endpoints.json");
 
-exports.fetchTopics = () => {
-  return db
-    .query(
-      `
+exports.fetchTopics = async () => {
+  const topicQuery = await db.query(
+    `
     SELECT * FROM topics
     `
-    )
-    .then(({ rows }) => {
-      return rows;
-    });
+  );
+  return topicQuery.rows;
 };
 
-exports.fetchArticles = (
+exports.fetchArticles = async (
   articleId,
   topicQuery,
   sortBy = "created_at",
@@ -34,206 +31,185 @@ exports.fetchArticles = (
   if (!validOrder.includes(orderBy)) {
     return Promise.reject({ status: 400, msg: "bad request" });
   }
-  return db
-    .query(`SELECT * FROM topics`)
-    .then(({ rows }) => {
-      if (topicQuery) {
-        const activeTopics = rows.map((row) => {
-          return row.slug;
-        });
-        if (!activeTopics.includes(topicQuery)) {
-          return Promise.reject({
-            status: 404,
-            msg: `${topicQuery} not found`,
-          });
-        }
-      }
-    })
-    .then(() => {
-      let queryStr = `
+  const topicsData = await db.query(`SELECT * FROM topics`);
+  if (topicQuery) {
+    const activeTopics = topicsData.rows.map((row) => {
+      return row.slug;
+    });
+    if (!activeTopics.includes(topicQuery)) {
+      return Promise.reject({
+        status: 404,
+        msg: `${topicQuery} not found`,
+      });
+    }
+  }
+  let queryStr = `
       SELECT articles.article_id, articles.title, articles.author,`;
 
-      if (articleId) {
-        queryStr += `articles.body,`;
-      }
+  if (articleId) {
+    queryStr += `articles.body,`;
+  }
 
-      queryStr += ` articles.topic, articles.created_at, articles.votes, COUNT(comments.article_id)::INT AS comment_count
+  queryStr += ` articles.topic, articles.created_at, articles.votes, COUNT(comments.article_id)::INT AS comment_count
       FROM articles
       LEFT JOIN comments ON comments.article_id = articles.article_id`;
 
-      const queryValues = [];
+  const queryValues = [];
 
-      if (articleId) {
-        queryStr += ` WHERE articles.article_id = $1`;
-        queryValues.push(articleId);
-      }
+  if (articleId) {
+    queryStr += ` WHERE articles.article_id = $1`;
+    queryValues.push(articleId);
+  }
 
-      if (topicQuery) {
-        queryStr += ` WHERE articles.topic = $1`;
-        queryValues.push(topicQuery);
-      }
+  if (topicQuery) {
+    queryStr += ` WHERE articles.topic = $1`;
+    queryValues.push(topicQuery);
+  }
 
-      queryStr += ` GROUP BY articles.article_id 
+  queryStr += ` GROUP BY articles.article_id 
       ORDER BY ${sortBy} ${orderBy};`;
 
-      return db.query(queryStr, queryValues);
-    })
-    .then(({ rows }) => {
-      if (rows.length === 0 && !topicQuery) {
-        return Promise.reject({ status: 404, msg: "not found" });
-      } else if (rows.length === 0 && topicQuery) {
-        return rows;
-      } else if (rows.length === 1) {
-        return rows[0];
-      } else {
-        return rows;
-      }
-    });
+  const fullQuery = await db.query(queryStr, queryValues);
+
+  if (fullQuery.rows.length === 0 && !topicQuery) {
+    return Promise.reject({ status: 404, msg: "not found" });
+  } else if (fullQuery.rows.length === 0 && topicQuery) {
+    return fullQuery.rows;
+  } else if (fullQuery.rows.length === 1) {
+    return fullQuery.rows[0];
+  } else {
+    return fullQuery.rows;
+  }
 };
 
-exports.fetchUsers = () => {
-  return db
-    .query(
-      `
+exports.fetchUsers = async () => {
+  const allUsers = await db.query(
+    `
       SELECT * FROM users
       `
-    )
-    .then(({ rows }) => {
-      return rows;
-    });
+  );
+
+  return allUsers.rows;
 };
 
-exports.updateArticles = (articleId, newVotes) => {
-  return db
-    .query(
-      `
+exports.updateArticles = async (articleId, newVotes) => {
+  const checkArticleData = await db.query(
+    `
     SELECT votes FROM articles
     WHERE article_id = $1;
     `,
-      [articleId]
-    )
-    .then(({ rows }) => {
-      if (rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "not found" });
-      }
-      const votes = rows[0].votes + newVotes;
-      return db.query(
-        `
+    [articleId]
+  );
+
+  if (checkArticleData.rows.length === 0) {
+    return Promise.reject({ status: 404, msg: "not found" });
+  }
+  const votes = checkArticleData.rows[0].votes + newVotes;
+  const updateArticleData = await db.query(
+    `
       UPDATE articles
       SET votes = $1
       WHERE article_id = $2
       RETURNING *
       `,
-        [votes, articleId]
-      );
-    })
-    .then(({ rows }) => {
-      return rows[0];
-    });
+    [votes, articleId]
+  );
+
+  return updateArticleData.rows[0];
 };
 
-exports.fetchCommentsOnArticle = (articleId) => {
+exports.fetchCommentsOnArticle = async (articleId) => {
   if (articleId.match(/\D/g) || articleId < 1) {
     return Promise.reject({ status: 400, msg: "bad request" });
   }
-  return db
-    .query(
-      `
+  const activeArticleIds = await db.query(
+    `
   SELECT * FROM articles
   `
-    )
-    .then(({ rows }) => {
-      const activeArticleIds = rows.map((row) => {
-        return row.article_id;
-      });
-      if (!activeArticleIds.includes(+articleId)) {
-        return Promise.reject({
-          status: 404,
-          msg: `article id ${articleId} not found`,
-        });
-      }
-    })
-    .then(() => {
-      return db.query(
-        `
+  );
+
+  const activeArticleIdsArray = activeArticleIds.rows.map((row) => {
+    return row.article_id;
+  });
+  if (!activeArticleIdsArray.includes(+articleId)) {
+    return Promise.reject({
+      status: 404,
+      msg: `article id ${articleId} not found`,
+    });
+  }
+
+  const activeComments = await db.query(
+    `
   SELECT * FROM comments 
   WHERE article_id = $1;
   `,
-        [articleId]
-      );
-    })
-    .then(({ rows }) => {
-      return rows;
-    });
+    [articleId]
+  );
+  return activeComments.rows;
 };
 
-exports.addCommentsOnArticle = (article_id, username, body) => {
+exports.addCommentsOnArticle = async (article_id, username, body) => {
   if (article_id.match(/\D/g) || article_id < 1) {
     return Promise.reject({ status: 400, msg: "bad request" });
   }
   if (body === undefined || username === undefined) {
     return Promise.reject({ status: 400, msg: "bad request" });
   }
-  return db
-    .query(
-      `
+  const activeUsersAndIds = await db.query(
+    `
   SELECT * FROM users
   JOIN articles ON articles.author = users.username;
   `
-    )
-    .then(({ rows }) => {
-      const activeArticleIds = rows.map((row) => {
-        return row.article_id;
-      });
-      const activeUsernames = rows.map((row) => {
-        return row.username;
-      });
-      return {
-        activeUsernames: !activeUsernames.includes(username),
-        activeArticleIds: !activeArticleIds.includes(+article_id),
-      };
-    })
-    .then(({ activeUsernames, activeArticleIds }) => {
-      if (!body || activeUsernames || activeArticleIds) {
-        return Promise.reject({ status: 404, msg: "not found" });
-      }
-      const timeStamp = new Date(Date.now());
-      return db.query(
-        `
+  );
+  const activeArticleIds = activeUsersAndIds.rows.map((row) => {
+    return row.article_id;
+  });
+  const activeUsernames = activeUsersAndIds.rows.map((row) => {
+    return row.username;
+  });
+  if (
+    !body ||
+    !activeUsernames.includes(username) ||
+    !activeArticleIds.includes(+article_id)
+  ) {
+    return Promise.reject({ status: 404, msg: "not found" });
+  }
+  const timeStamp = new Date(Date.now());
+  const postComment = await db.query(
+    `
   INSERT INTO comments
   (votes, body, author, article_id, created_at)
   VALUES
   ('0', $1, $2, $3, $4)
   RETURNING *;
   `,
-        [body, username, article_id, timeStamp]
-      );
-    })
-    .then(({ rows }) => {
-      return rows[0];
-    });
+    [body, username, article_id, timeStamp]
+  );
+
+  return postComment.rows[0];
 };
 
-exports.removeCommentById = (commentId) => {
+exports.removeCommentById = async (commentId) => {
   if (commentId.match(/\D/g) || commentId < 1) {
     return Promise.reject({ status: 400, msg: "bad request" });
   }
-  return db
-    .query(`SELECT * FROM comments WHERE comment_id = $1`, [commentId])
-    .then(({ rows }) => {
-      if (!rows[0]) {
-        return Promise.reject({ status: 404, msg: "not found" });
-      }
-    })
-    .then(() => {
-      return db.query(
-        `
+  const activeArticleIds = await db.query(
+    `SELECT * FROM comments WHERE comment_id = $1`,
+    [commentId]
+  );
+
+  if (!activeArticleIds.rows[0]) {
+    return Promise.reject({ status: 404, msg: "not found" });
+  }
+
+  const deleteComment = await db.query(
+    `
   DELETE FROM comments
   WHERE comment_id = $1;
   `,
-        [commentId]
-      );
-    });
+    [commentId]
+  );
+  return deleteComment;
 };
 
 exports.fetchApi = () => {
