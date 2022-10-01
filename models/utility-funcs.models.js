a2ebbfcb0f87554ae0249eea3f9039e9ddd66b21
sam-c-f <sam_friedman@hotmail.co.uk>
2022-10-01 09:52:59 +0100

    removed the textcheck for author as can have numbers in name and will be checked on add user

--- models/utility-funcs.models.js
+++ models/utility-funcs.models.js
@@ -11,7 +11,6 @@ exports.onlyPositiveIntegers = (param) => {
 };
 
 exports.calculatePage = (p, limit, rowCount) => {
-  console.log(rowCount);
   if (+p === 1 || +limit > rowCount) {
     page = 0;
   } else if (+p === 2) {
